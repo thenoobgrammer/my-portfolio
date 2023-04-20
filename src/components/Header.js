0b482e0fcaca006f59ddd6f14f5a133579db7f18
Antoine Hakim <tonyown10@gmail.com>
2023-04-20 16:16:36 -0400

    * updated Headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,52 +9,47 @@ const Header = () => {
   const [open, setOpen] = React.useState(false)
 
   return (
-    <div className='py-8 bg-white md:bg-none md:fixed w-full'>
-      <div className='container mx-auto'>
-        <div className='relative md:flex md:justify-between md:items-center'>
+    <>
+      <div className={`${open ? 'opacity-25 fixed inset-0 z-3 bg-black' : ''}`}></div>
+      <div className='bg-white md:bg-none fixed w-full z-50'>
+        <div className='md:flex md:justify-between md:items-center py-8 container mx-auto'>
           <a href='#'>
             <img src={Logo} alt='' />
           </a>
-          <div className={`
-            ${open ? 'hidden' : 'visible'}
-            static flex flex-col items-end my-7 ease-linear transition-height duration-150
-            md:flex md:flex-row md:gap-x-6 md:my-0 
-            lg:flex lg:flex-2 lg:gap-10`
-          }
+          <div className={`relative flex flex-col items-end my-7 ease-linear transition-height duration-300 ${open ? '-top-none xs:h-auto' : 'xs:h-0 -top-[500px]'} md:h-fit md:static md:flex md:flex-row md:gap-x-6 md:my-0 lg:flex lg:flex-2 lg:gap-10`}
           >
             {Links.map(({ path, offset = 0, description, icon }, idx) => (
-              <Link
-                to={path}
-                offset={offset}
-                activeClass='active'
-                smooth={true}
-                spy={true}
-                className='
-                  flex flex-col items-center justify-center cursor-pointer 
-                  text-primary w-[60px] h-[60px] href
-                  '
-              >
-                {icon}
-                <span className='text-[22px] font-medium'>{description}</span>
-              </Link>
+              <div className='border-b-2 border-accent/40 mb-4 flex md:border-none md:mb-0 md:flex-none'>
+                <Link
+                  to={path}
+                  offset={offset}
+                  activeClass='active'
+                  smooth={true}
+                  spy={true}
+                  className='flex flex-col items-center justify-center cursor-pointer 
+                  text-primary w-[60px] h-[60px] href'
+                >
+                  {icon}
+                  <span className='text-[22px] font-medium'>{description}</span>
+                </Link>
+              </div>
             ))}
           </div>
           <div className='
-            flex absolute top-0 right-0 gap-x-3 
-            md:relative 
+            absolute flex top-8 right-8 gap-x-3 
+            md:relative md:top-0
             lg:items-center lg:flex lg:flex-3 lg:gap-5'
           >
             <Modale title='My CV'>
               My CV
             </Modale>
-            <button className='lg:hidden md:hidden
-              btn-not-background text-primary text-[40px]' onClick={() => setOpen(!open)}>
+            <button className='lg:hidden md:hidden btn-not-background text-primary text-[40px]' onClick={() => setOpen(!open)}>
               <BiMenuAltRight />
             </button>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 };
 
